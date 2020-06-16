b0911ec6ea59660cf9659124800e8f8c1448b4e9
chibaf <chibaf@mac.com>
2020-06-16 19:15:03 +0900

    Add files via upload

--- read_serial02d.py
+++ read_serial02d.py
@@ -16,9 +16,7 @@ while True:
     itime=0
     line = ser.readline()
     match = regex.findall(str(line))   # extracting number from strings
-    # "match[4]+"."+match[5]" is the first Tc value, ..., "match[22]+"."+match[23]" is the tenth Tc value.
     f1=match[4]+"."+match[5]+", "+match[6]+"."+match[7]+", "+match[8]+"."+match[9]+", "+match[10]+"."+match[11]+", "+match[12]+"."+match[13]+", "+match[14]+"."+match[15]+", "+match[16]+"."+match[17]+", "+match[18]+"."+match[19]+", "+match[20]+"."+match[21]+", "+match[22]+"."+match[23]
-    # match[1] is minutes, match[2] is seconds, match[3] is sub seconds.
     sec=float(match[1])*60.0+float(match[2])+float(match[3])*0.1
     data.append(itime*0.1)    # set time and temps to data
     data.append(float(match[4]+"."+match[5]))
