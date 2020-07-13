8a7e72f132870e1cc7cbd36404d375bfedfb6150
chibaf <chibaf@mac.com>
2020-07-13 22:52:32 +0900

    Add files via upload

--- read_serial02d.py
+++ read_serial02d.py
@@ -13,10 +13,9 @@ y=[0]*100
 data=[];itime=0
 while True:
   try:
-    itime=0
     line = ser.readline()
     match = regex.findall(str(line))   # extracting number from strings
-    f1=match[4]+"."+match[5]+", "+match[6]+"."+match[7]+", "+match[8]+"."+match[9]+", "+match[10]+"."+match[11]+", "+match[12]+"."+match[13]+", "+match[14]+"."+match[15]+", "+match[16]+"."+match[17]+", "+match[18]+"."+match[19]+", "+match[20]+"."+match[21]+", "+match[22]+"."+match[23]
+    f1=str(itime*0.1)+", "+match[4]+"."+match[5]+", "+match[6]+"."+match[7]+", "+match[8]+"."+match[9]+", "+match[10]+"."+match[11]+", "+match[12]+"."+match[13]+", "+match[14]+"."+match[15]+", "+match[16]+"."+match[17]+", "+match[18]+"."+match[19]+", "+match[20]+"."+match[21]+", "+match[22]+"."+match[23]
     sec=float(match[1])*60.0+float(match[2])+float(match[3])*0.1
     data.append(itime*0.1)    # set time and temps to data
     data.append(float(match[4]+"."+match[5]))
@@ -30,10 +29,10 @@ while True:
     data.append(float(match[20]+"."+match[21]))
     data.append(float(match[22]+"."+match[23]))
     if(match[3]=="0"):
-      print(str(sec)+":"+f1)
+      print(str(sec)+": "+f1)
     f.write(str(sec)+", "+f1+"\n")
     x=range(0, 100, 1)
-    y.insert(0, data[3])  # Tc No.6
+    y.insert(0, data[1])  # Tc No.6
     y.pop(100)
     clf()
     ylim(0, 1000)
@@ -41,7 +40,7 @@ while True:
     pause(0.05) 
     itime=itime+1
     data=[]
-    ser.flush()
+#    ser.flush()
   except KeyboardInterrupt:
     print(str(line))
     print ('exiting')
